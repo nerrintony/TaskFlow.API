6b3a70bb7ed9bcdfa55b86952f916ea8d2f33373
nerrin <nirrentony@gmail.com>
2026-04-11 11:42:04 +0530

    crud operations

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,7 +7,7 @@ namespace TaskFlow.API.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private static List<User> user = new List<User>
+        private static List<User> users = new List<User>
         {
             new User { Id = 1, Name = "Nerrin", Email = "nerrin@yopmail.com" },
             new User { Id = 2, Name = "TOny", Email = "tony@yopmail.com" },
@@ -16,7 +16,41 @@ namespace TaskFlow.API.Controllers
         [HttpGet]
         public ActionResult GetUser()
         {
-            return Ok(user);
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            user.Id = users.Count + 1;
+            users.Add(user);
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+                return NotFound();
+            else
+                return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUserById(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+                return NotFound();
+            else
+            {
+                users.Remove(user);
+                return Ok();
+            }
         }
     }
 }
