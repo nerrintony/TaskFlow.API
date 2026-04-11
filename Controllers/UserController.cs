160ed841c91af17cdbacb8992ebd9a9170480fb2
nerrin <nirrentony@gmail.com>
2026-04-11 11:26:38 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskFlow.API.Models;
+
+namespace TaskFlow.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static List<User> user = new List<User>
+        {
+            new User { Id = 1, Name = "Nerrin", Email = "nerrin@yopmail.com" },
+            new User { Id = 2, Name = "TOny", Email = "tony@yopmail.com" },
+        };
+
+        [HttpGet]
+        public ActionResult GetUser()
+        {
+            return Ok(user);
+        }
+    }
+}
