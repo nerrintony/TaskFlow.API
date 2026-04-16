8957cca32fe37a3a6c450599d075c5dc0f215644
nerrin <nirrentony@gmail.com>
2026-04-16 19:50:29 +0530

    request migration updated

---
+++ Migrations/20260416103556_AddUserRequestRelation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRequestRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_CreatedByUserId",
+                table: "Requests",
+                column: "CreatedByUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_CreatedByUserId",
+                table: "Requests",
+                column: "CreatedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_CreatedByUserId",
+                table: "Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Requests_CreatedByUserId",
+                table: "Requests");
+        }
+    }
+}
--- Models/Request.cs
+++ Models/Request.cs
@@ -8,6 +8,6 @@
         public string Status { get; set; } = "Draft";
         public DateTime CreateAt { get; set; } = DateTime.UtcNow;
         public int CreatedByUserId { get; set; }
-
+        public User CreatedByUser { get; set; }
     }
 }
